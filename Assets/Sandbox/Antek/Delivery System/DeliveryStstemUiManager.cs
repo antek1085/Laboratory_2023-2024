e53cf55b440df7917257afc66a853d60bd14f492
antek1085 <antek1085@o2.pl>
2023-12-20 17:22:34 +0100

    cleanup of objects on scene

--- Assets/Sandbox/Antek/Delivery System/DeliveryStstemUiManager.cs
+++ Assets/Sandbox/Antek/Delivery System/DeliveryStstemUiManager.cs
@@ -15,10 +15,11 @@ public class DeliveryStstemUiManager : MonoBehaviour
     [SerializeField] DeliverySystem time;
 
     float timer;
-    Sprite spriteDisplay;
+    private Sprite spriteDisplay;
 
    [SerializeField] GameObject slider;
    [SerializeField] Image image;
+   private bool isValueChanged;
 
    void Update()
     {
@@ -30,10 +31,18 @@ public class DeliveryStstemUiManager : MonoBehaviour
             spriteDisplay = time.deliveryItemList[numberOnUi].sprite;
             
             slider.GetComponent<Slider>().value = timer;
+            
+            // if (isValueChanged == false)
+            // {
+            //     isValueChanged = true;
+            //     slider.GetComponent<Slider>().maxValue = timer;
+            // }
+            
             image.sprite = spriteDisplay;
         }
         else
         {
+            isValueChanged = false;
             image.enabled = false;
             slider.SetActive(false);
         }
